1d2fe9bd49bab8eadcf35243914a5cf422da8ed8
bohdanpotuzhnyi <72936384+bohdanpotuzhnyi@users.noreply.github.com>
2021-05-01 11:56:42 +0300

    multiCPU and jacobi function

---
+++ scripts/jacobi.js
@@ -0,0 +1,71 @@
+var s = "";
+function jacobi(a){
+    var b = new Object(); b[0] = 1; b[1] = 1; b[2] = 1; // Array(1,1,1);
+    var sign = a[0];
+    var m = a[1];
+    var n = a[2];
+    if( sign > 0 ) s+=("("+m+"/"+n+") = ");
+    if( sign < 0 ) s+=("-("+m+"/"+n+") = ");
+    if( m > n-1 ) {
+        m = m % n;
+        if( sign > 0 ) s+=("("+m+"/"+n+") = ");
+        if( sign < 0 ) s+=("-("+m+"/"+n+") = ");
+    }
+    m = m % n;
+    if( m == 0 ){
+        s+=("0");
+        return 0;
+    }
+    if( m == 1 ) {
+        if( sign > 0 ) { s+=("1"); return 1; }
+        s+=("-1"); return -1;
+    }
+    var J2n = 1;
+    if( (3 == (n%8)) || (5 == (n%8)) ) J2n = -1;
+    if( 0 == (m%2) ) {
+        b[0] = J2n*sign;
+        b[1] = m/2;
+        b[2] = n;
+        return jacobi(b);
+    }
+    if( (3 == (n%4)) && (3 == (m%4)) ) {
+        b[0] = -sign;
+        b[1] = n % m;
+        b[2] = m;
+        return jacobi(b);
+    } else {
+        b[0] = sign;
+        b[1] = n % m;
+        b[2] = m;
+        return jacobi(b);
+    }
+}
+
+module.exports.jac = (m,n) => {
+
+    if( 0 == n % 2 ){
+        alert("The bottom number must be odd");
+        return;
+    }
+    if( (n < 0) || (m < 0) ){
+        alert("No negative numbers please");
+        return;
+    }
+    var a = new Object(); a[0] = 1; a[1] = m; a[2] = n; // Array(1,m,n);
+
+    var ans = jacobi(a);
+
+    if( (ans == 1) && (n < 30000) ) {
+        res = false;
+        for( var i = 1; i < n/2; i++ ) {
+            if( 0 == (((i*i)-m) % n) ) {
+                res = true;
+                s+=("\nA residue: "+m+" = "+i+"^2 mod "+n);
+                break;
+            }
+        }
+        if (res == false) s+=("Not a residue");
+    }
+
+    return s;
+}
--- test.js
+++ test.js
@@ -1,12 +1,35 @@
 const http = require('http');
+const os = require("os");
+const cluster = require("cluster");
 const { Telegraf } = require('telegraf');
 
 const token = '1691713249:AAG6g1uwDsNs6aaI6uX6GQEOnWX5zaXjKwc';
+const bot = new Telegraf(token);
+const PORT = 8100;
 
-const bot = new Telegraf(token)
+const clusterWorkerSize = os.cpus().length;
+
+bot.telegram.setWebhook("https://api.queuebot.me");
 
 bot.on('text', (ctx) => {
     ctx.reply("fck u");
 })
 
-http.createServer(bot.webhookCallback('/')).listen(8100, '127.0.0.1');
+if (clusterWorkerSize > 1) {
+    if (cluster.isMaster) {
+        for (let i=0; i < clusterWorkerSize; i++) {
+            cluster.fork()
+        }
+
+        cluster.on("exit", function(worker) {
+            console.log("Worker", worker.id, " has exitted.")
+        })
+    } else {
+
+        http.createServer(bot.webhookCallback('/')).listen(8100, '127.0.0.1');
+        console.log(`Express server listening on port ${PORT} and worker ${process.pid}`);
+    }
+} else {
+    http.createServer(bot.webhookCallback('/')).listen(8100, '127.0.0.1');
+    console.log(`Express server listening on port ${PORT} with the single worker ${process.pid}`);
+}
