46b7df9c0e5717e909f7c6fde6a3114cc9added7
bohdanpotuzhnyi <72936384+bohdanpotuzhnyi@users.noreply.github.com>
2021-05-04 13:53:13 +0300

    jacobi calculation

--- test.js
+++ test.js
@@ -2,17 +2,25 @@ const http = require('http');
 const os = require("os");
 const cluster = require("cluster");
 const { Telegraf } = require('telegraf');
+const jacobi = require('./scripts/jacobi.js');
 
 const token = '1691713249:AAG6g1uwDsNs6aaI6uX6GQEOnWX5zaXjKwc';
 const bot = new Telegraf(token);
-const PORT = 8100;
 
 const clusterWorkerSize = os.cpus().length;
 
 bot.telegram.setWebhook("https://api.queuebot.me");
 
 bot.on('text', (ctx) => {
-    ctx.reply("fck u");
+    var s = ctx.message.text;
+    const ss = s.split(' ')
+    if(ss[0] === "Jacobi" || ss[0]==="jacobi"){
+        var m1=parseInt(ss[1])
+        var n1=parseInt(ss[2])
+
+        s = jacobi.jac(m1, n1)
+    }
+    ctx.reply(s);
 })
 
 if (clusterWorkerSize > 1) {
@@ -27,9 +35,9 @@ if (clusterWorkerSize > 1) {
     } else {
 
         http.createServer(bot.webhookCallback('/')).listen(8100, '127.0.0.1');
-        console.log(`Express server listening on port ${PORT} and worker ${process.pid}`);
+        console.log(`Express server listening on port 8100 and worker ${process.pid}`);
     }
 } else {
     http.createServer(bot.webhookCallback('/')).listen(8100, '127.0.0.1');
-    console.log(`Express server listening on port ${PORT} with the single worker ${process.pid}`);
+    console.log(`Express server listening on port 8100 with the single worker ${process.pid}`);
 }
