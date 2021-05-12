c7b67c27236364dcf71e4b0689951c50f4e63468
Evgeniy Churikov <zhenya.churikov@gmail.com>
2021-05-12 12:30:27 +0300

    Remove old code

--- test.js
+++ test.js
@@ -12,15 +12,6 @@ const token = '1681110137:AAEmEwDuJFK-lps4pD4uL6LKPrSE3zRtACI';
 const bot = new Telegraf(token);
 const clusterWorkerSize = os.cpus().length;
 
-const whitelist = [
-    341421484,
-    497327654
-];
-
-var dir = "/home/queuebot/api.queuebot.me/temp/"
-
-var images = [];
-
 bot.telegram.setWebhook("https://api.queuebot.me");
 
 function getMessage(path) {
@@ -213,110 +204,6 @@ async function rootmod_problem(ctx) {
         await fs.promises.rm(`temp/${id}`, {recursive: true});
     }
 }
-
-bot.on('text', async(ctx) => {
-
-    var dir = `/home/queuebot/api.queuebot.me/temp/${ctx.message.from.id}`;
-
-    if (!fs.existsSync(dir)){
-        fs.mkdirSync(dir);
-    }
-
-    var id = ctx.message.from.id
-
-    var s = ctx.message.text
-
-    var ss = s.split(' ')
-    if(whitelist.includes(id)){
-        switch(ss[0].toLowerCase()){
-            case "gcd":
-                var m1 = parseInt(ss[1])
-                var n1 = parseInt(ss[2])
-                if((m1 > 1) && (n1 > 1)) {
-                    s = basic.display_ea(m1, n1)
-                    await latex.writetex(id, s)
-                    await latex.compile(id);
-                    await ctx.replyWithPhoto({source: `temp/${ctx.message.from.id}/solving1.png`})
-                }else ctx.reply("Введи нормальні числа")
-                break;
-            case "jacobi":
-                var m1 = parseInt(ss[1])
-                var n1 = parseInt(ss[2])
-                if((m1 > 1) && (n1 > 2) && (n1 % 2 != 0)) {
-                    s = jacobi.jac_custom(m1, n1)
-                    await latex.writetex(id, s.s)
-                    await latex.compile(id);
-                    await ctx.replyWithPhoto({source: `temp/${ctx.message.from.id}/solving1.png`})
-
-                }else ctx.reply("Введи нормальні числа")
-                break;
-            case "rootmod":
-                //ctx.reply("rootmod")
-                var a = parseInt(ss[1])
-                var p = parseInt(ss[2])
-                if((a > 0) && (p > 2)){
-                    if(basic.prime(p)){
-                        jacs = jacobi.jac_custom(a, p)
-                        //latex.wts(ctx.message.from.id, jacs.s)
-                        ctx.reply(`${jacs.res} ${a} ${p}`)
-                        await exec(`/home/queuebot/api.queuebot.me/scripts/cpp/dm/rootmod ${jacs.res} ${a} ${p} /home/queuebot/api.queuebot.me/temp/${id}/solving1.tex`);
-                        var data = fs.readFileSync(`/home/queuebot/api.queuebot.me/temp/${id}/solving1.tex`, 'utf8');
-                        jacs.s += data;
-                        latex.wts(ctx.message.from.id, jacs.s)
-                        await latex.compile(id);
-                        await ctx.replyWithPhoto({source: `temp/${ctx.message.from.id}/solving1.png`})
-
-                    } else ctx.reply(`${p}-не просте`)
-                }else{
-                    ctx.reply("Введіть p - більше за 2")
-                }
-                break;
-            case "lineareq":
-                await exec(`./scripts/cpp/dm/linearequation ${ss[1]} ${ss[2]} ${ss[3]} ./temp/${id}/solving.tex`);
-
-                await latex.compile(id);
-                await ctx.replyWithPhoto({source: `temp/${ctx.message.from.id}/solving1.png`})
-                await ctx.replyWithPhoto({source: `temp/${ctx.message.from.id}/solving2.png`})
-                break;
-            default:
-                ctx.reply(s)
-                break;
-        }
-    } else {
-        switch(ss[0].toLowerCase()) {
-            case "/help":
-                break
-            case "rootmod":
-                var a = parseInt(ss[1])
-                var p = parseInt(ss[2])
-                if ((a > 0) && (p > 2)) {
-                    ctx.reply("rootmod")
-                } else ctx.reply("Введи нормальні числа")
-                break;
-            case "gcd":
-                var m1 = parseInt(ss[1])
-                var n1 = parseInt(ss[2])
-                if ((m1 > 0) && (n1 > 0)) {
-                    ctx.reply("gcd")
-                } else {
-                    "Введи нормальні числа"
-                }
-                break
-            case "jacobi":
-                var m1 = parseInt(ss[1])
-                var n1 = parseInt(ss[2])
-                if ((m1 > 0) && (n1 > 1) && (n1 % 2 != 0)) {
-                    ctx.reply("jacobi")
-                } else ctx.reply("Введи нормальні числа")
-            case "lineraeq":
-                ctx.reply("lineareq")
-                break
-            default:
-                ctx.reply(s)
-                break
-        }
-    }
-})
 
 if (clusterWorkerSize > 1) {
     if (cluster.isMaster) {
